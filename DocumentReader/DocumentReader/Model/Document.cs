4de8e8e3e12e40c9f4fb5f2803b2c65af1da3b17
jan <winklerjan@protonmail.com>
2023-03-05 18:34:48 +0100

    refactor initial code

---
+++ DocumentReader/DocumentReader/Model/Document.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentReader.Model
+{
+    public class Document
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+
+        public Document(string title, string text)
+        {
+            Text = text;
+            Title = title;
+        }
+    }
+}
--- DocumentReader/DocumentReader/Program.cs
+++ DocumentReader/DocumentReader/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
+using DocumentReader.Model;
+using Newtonsoft.Json;
+namespace DocumentReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the name of the source file: ");
+            string sourceFileName = Console.ReadLine();
+            string sourceFilePath = Path.Combine("..\\..\\..\\SourceFiles", sourceFileName);
+
+            Console.Write("Enter the name of the target file: ");
+            string targetFileName = Console.ReadLine();
+            string targetFilePath = Path.Combine("..\\..\\..\\TargetFiles", targetFileName);
+
+            string input;
+
+            try
+            {
+                FileStream sourceStream = File.Open(sourceFilePath, FileMode.Open);
+                StreamReader reader = new StreamReader(sourceStream);
+                input = reader.ReadToEnd();
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                // TODO throw more detailed exceptions
+                throw new Exception(ex.Message);
+            }
+
+            XDocument xdoc = XDocument.Parse(input);
+
+            string title = xdoc.Root.Element("title").Value;
+            string text = xdoc.Root.Element("text").Value;
+
+            var doc = new Document(title, text);
+
+            string serializedDoc = JsonConvert.SerializeObject(doc);
+
+            // TODO wrap in a try/catch statement
+            using (FileStream targetStream = File.Open(targetFilePath, FileMode.Create, FileAccess.Write))
+            {
+                using (var sw = new StreamWriter(targetStream))
+                {
+                    sw.Write(serializedDoc);
+                }
+            }
+        }
+    }
+}
+
